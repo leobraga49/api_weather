9a7d4969230fb8cafe74c11b3d95a5a656c2c8b9
leobraga49 <leonardobarrosbraga@gmail.com>
2023-06-30 15:29:05 -0300

    Include constructor

--- src/main/java/com/api/api_weather/model/Weather.java
+++ src/main/java/com/api/api_weather/model/Weather.java
@@ -1,9 +1,12 @@
 package com.api.api_weather.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Weather {
     private String main;
     private String description;
+
 }
